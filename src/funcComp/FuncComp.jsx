14fa9a2e574ec89e77a2c7b0f3d27bb58b87b78e
olafro <olka.be@gmail.com>
2020-10-21 16:05:55 +0200

    react functional component

--- src/funcComp/FuncComp.jsx
+++ src/funcComp/FuncComp.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FuncComp() {
+  const [form, setForm] = useState({
+    fName: "",
+    lName: "",
+    birthday: "",
+    city: "",
+  });
   return (
     <div className="container col-10">
       <div className="flex-column d-flex bg-warning border p-5">
-        <input type="text" name="fName" placeholder="First Name" />
-        <input type="text" name="lName" placeholder="Last Name" />
-        <input type="date" name="birthday" placeholder="Birthday" />
-        <input type="text" name="city" placeholder="City" />
+        <input
+          onChange={(e) => {
+            setForm({ ...form, [e.target.name]: e.target.value });
+          }}
+          type="text"
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={(e) => {
+            setForm({ ...form, [e.target.name]: e.target.value });
+          }}
+          type="text"
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={(e) => {
+            setForm({ ...form, [e.target.name]: e.target.value });
+          }}
+          type="date"
+          name="birthday"
+          placeholder="Birthday"
+        />
+        <input
+          onChange={(e) => {
+            setForm({ ...form, [e.target.name]: e.target.value });
+          }}
+          type="text"
+          name="city"
+          placeholder="City"
+        />
         <div className="btn btn-primary my-3">Submit</div>
       </div>
     </div>
